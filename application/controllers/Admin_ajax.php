0f047358b90709d13f16bad02b4ba85dfcbb0818
Anderson Laverde Gracia <ander.laverde.dev@gmail.com>
2020-03-17 18:25:05 -0500

    fix( nameStudent FIx)

--- application/controllers/Admin_ajax.php
+++ application/controllers/Admin_ajax.php
@@ -21,9 +21,6 @@ class Admin_ajax extends CI_Controller {
 		$sql = $this->db->select('name')->where('id',$idUser)->get('users')->result()[0];
 		$b['content'] = $sql;
 		$b['response'] = 2;
-		echo '<pre>';
-			var_dump($idUser);
-		echo '</pre>';
 		echo json_encode($b);
 	}
 	public function nameSession(){
