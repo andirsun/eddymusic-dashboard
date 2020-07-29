d2bd2aaf8ec44cfa84c1747070082e7694762114
Anderson Laverde Gracia <ander.laverde.dev@gmail.com>
2020-07-28 21:59:16 -0500

    chore(): upgrade jquery and Boostrap to last version

--- application/views/index.php
+++ application/views/index.php
@@ -7,11 +7,12 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
   <title>EddyMusic Academy</title>
-  <!-- Bootstrap -->
-  <link href="<?php echo base_url() ?>assets/css/bootstrap.min.css" rel="stylesheet"> <!-- la base url es la pagina y el resto es la ruta donde se encuentra-->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+    integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
   <link rel="stylesheet" href="<?php echo base_url() ?>assets/css/main.css">
   <link rel="stylesheet" href="<?php echo base_url() ?>assets/css/login.css">
-  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css"
+    integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
   <link rel="icon" href="<?php echo base_url()?>assets/images/miniicono.png">
 
 </head>
@@ -19,42 +20,48 @@
 <body>
   <script>
     var base_url = '<?php echo base_url(); ?>';
-    console.log("base url "+base_url);
+    console.log("base url " + base_url);
   </script>
   <div class="container">
-	<div class="d-flex justify-content-center h-100">
-		<div class="card">
-			<div class="card-header">
-				<h3>Ingresar</h3>
-			</div>
-			<div class="card-body">
-				<form id="formLogin">
-					<div class="input-group form-group">
-						<div class="input-group-prepend">
-							<span class="input-group-text"><i class="fas fa-user"></i></span>
-						</div>
-						  <input id="username" name="user" type="text" class="form-control" placeholder="Correo" required="required">
+    <div class="d-flex justify-content-center h-100">
+      <div class="card">
+        <div class="card-header">
+          <h3>Ingresar</h3>
+        </div>
+        <div class="card-body">
+          <form id="formLogin">
+            <div class="input-group form-group">
+              <div class="input-group-prepend">
+                <span class="input-group-text"><i class="fas fa-user"></i></span>
+              </div>
+              <input id="username" name="user" type="text" class="form-control" placeholder="Correo"
+                required="required">
             </div>
             <div class="input-group form-group">
               <div class="input-group-prepend">
                 <span class="input-group-text"><i class="fas fa-key"></i></span>
               </div>
-              <input id="contraseña1" name="pass" type="password" class="form-control" placeholder="Constraseña" required="required">
+              <input id="contraseña1" name="pass" type="password" class="form-control" placeholder="Constraseña"
+                required="required">
             </div>
             <div class="form-group">
               <button type="submit" class="btn btn-block ml-auto login_btn">Iniciar</button>
             </div>
             <div class="alert alert-danger py-2" style="display: none;" id="msg-login">
-            	<h4 class="m-0 text-center" id="txt-login-msg"></h4>
+              <h4 class="m-0 text-center" id="txt-login-msg"></h4>
             </div>
-				</form>
-			</div>
-		</div>
-	</div>
-</div>  
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- JS, Popper.js, and jQuery -->
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+  <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
   <script src="<?php echo base_url(); ?>assets/js/jquery-3.3.1.min.js" type="text/javascript"></script>
-  <script src="<?php echo base_url(); ?>assets/js/bootstrap.min.js"></script>
+  <script src="<?php echo base_url(); ?>assets/js/bootstrap.min.js"></script> -->
   <script src="<?php echo base_url(); ?>assets/js/main.js?<?php echo strtotime(date('Y-m-d H:i:s')) ?>"></script>
   <script src="<?php echo base_url(); ?>assets/js/admin/login.js"></script>
 </body>
--- application/views/prelogin.php
+++ application/views/prelogin.php
@@ -4,27 +4,23 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Academia Eddy Music</title>
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <!-- CSS only -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
     <link rel="stylesheet" href="<?php echo base_url() ?>assets/css/prelogin.css">
     <link rel="stylesheet" href="<?php echo base_url() ?>assets/css/login.css">
     <link rel="icon" href="<?php echo base_url()?>assets/images/miniicono.png">
-    <title>Academia Eddy Music</title>
 
   </head>
   <body >
-    <audio autoplay src="<?php echo base_url()?>assets/music/intro.mp3"></audio>
+    <!-- <audio autoplay src="<?php echo base_url()?>assets/music/intro.mp3"></audio> -->
     <video autoplay muted loop id="myVideo">
       <source src="<?php echo base_url()?>assets/videos/guitarra.webm" type="video/mp4">
     </video>
     <div class="container">
       <div class="row justify-content-center align-items-center minh-100">
-        <!--<div class="col">
-          <video id="video"src="./assets/video2.mp4" muted autoplay loop ></video>
-        </div>
-        background="./assets/fondo.jpg"
-      -->
+  
         <div class="col">
           <div class="card" style="width: 18rem;">
             <img class="card-img-top" src="<?php echo base_url() ?>assets/images/ibague.jpg" alt="Card image cap" height="200">
@@ -57,12 +53,11 @@
         </div>
       </div>
     </div>
-
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <!-- JS, Popper.js, and jQuery -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+  
   </body>
 
 
